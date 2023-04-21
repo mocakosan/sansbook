cd328cb7349b1226c53cc64e6d650eeebae4dfbe
js <js@172.20.nate.com>
2023-04-21 18:19:54 +0900

    remove: backurl1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f2588aaf3b7f6aaeec3d";
+/******/ 	var hotCurrentHash = "2bb7b8a1fc1be814192a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/imagesZoom/index.js
+++ components/imagesZoom/index.js
@@ -33,10 +33,7 @@ const ImagesZoom = ({ images, onClose }) => {
           >
             {images.map((v) => (
               <ImgWrapper key={v.src}>
-                <img
-                  src={`${v.src.replace(/\/thumb\//, "/original/")}`}
-                  alt={v.src}
-                />
+                <img src={`${v.src}`} alt={v.src} />
               </ImgWrapper>
             ))}
           </Slick>
