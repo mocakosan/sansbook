956368c88fa67dd28a23b119eaf40067366e5807
js <js@172.20.nate.com>
2023-04-24 17:01:26 +0900

    Update: Postform.js

--- components/PostForm.js
+++ components/PostForm.js
@@ -41,7 +41,7 @@ const PostForm = () => {
     console.log("images", e.target.files);
     const imageFormData = new FormData();
     [].forEach.call(e.target.files, (f) => {
-      imageFormData.append("image", f);
+      imageFormData.append("Images", f);
     });
     dispatch({
       type: UPLOAD_IMAGES_REQUEST,
