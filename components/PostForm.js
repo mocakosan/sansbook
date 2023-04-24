fec9eb22a18c1eed0af15988e196e281bc55f30c
js <js@jsui-noteubug.local>
2023-04-25 01:14:52 +0900

    Update: PostForm.js12

--- components/PostForm.js
+++ components/PostForm.js
@@ -26,7 +26,7 @@ const PostForm = () => {
     }
     const formData = new FormData();
     imagePaths.forEach((p) => {
-      formData.append("image", p);
+      formData.append("Images", p);
     });
     formData.append("content", text);
     return dispatch({
